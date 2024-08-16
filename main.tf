1636d340b411e097562b610c913037b59a97cee0
Manikanta Sadurla <95267560+manikanta-sadurla@users.noreply.github.com>
2024-08-16 15:57:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,10 +20,6 @@ provider "aws" {
 
 provider "awsutils" {
   region = var.region
-  # Uncomment and configure this if needed
-  # assume_role {
-  #   role_arn = local.environment_role[var.environment]
-  # }
 }
 
 provider "aws" {
@@ -37,13 +33,13 @@ provider "aws" {
 ##################### Source Account #################
 # Existing Transit Gateway in the source account
 resource "aws_ec2_transit_gateway" "transit_gateway" {
-  provider                         = aws
-  amazon_side_asn                  = var.transit_gateway_asn
-  auto_accept_shared_attachments   = "enable"
-  default_route_table_association  = "enable"
-  default_route_table_propagation  = "enable"
-  dns_support                      = "enable"
-  vpn_ecmp_support                 = "enable"
+  provider                        = aws
+  amazon_side_asn                 = var.transit_gateway_asn
+  auto_accept_shared_attachments  = "enable"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+  dns_support                     = "enable"
+  vpn_ecmp_support                = "enable"
 
   tags = {
     Name = var.transit_gateway_name
@@ -70,6 +66,33 @@ resource "aws_ram_resource_association" "transit_gateway" {
   resource_arn       = aws_ec2_transit_gateway.transit_gateway.arn
 }
 
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "source" {
+
+  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
+  vpc_id             = var.source_vpc_id
+
+  subnet_ids = var.source_subnet_ids
+
+  dns_support  = "enable"
+  ipv6_support = "disable"
+
+  tags = {
+    Name = "TransitGateway-VPC-Attachment"
+  }
+  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+}
+
+resource "aws_route" "source" {
+  for_each               = toset(var.source_route_table_ids)
+  route_table_id         = each.value
+  destination_cidr_block = var.destination_cidr_block
+  transit_gateway_id     = aws_ec2_transit_gateway.transit_gateway.id
+
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.source]
+}
+
+
 ################### Target Acconunt #####################################
 # Accept the share in the target account
 #########################################################################
@@ -80,26 +103,28 @@ resource "aws_ram_resource_share_accepter" "transit_gateway" {
   depends_on = [aws_ram_principal_association.target_account]
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   provider           = aws.target
   transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
   vpc_id             = var.vpc_id
 
   subnet_ids = var.subnet_ids
 
-  dns_support = "enable"
+  dns_support  = "enable"
   ipv6_support = "disable"
 
   tags = {
     Name = "TransitGateway-VPC-Attachment"
   }
+  depends_on = [aws_ec2_transit_gateway.transit_gateway]
 }
 
-resource "aws_route" "example" {
-  provider            = aws.target
-  route_table_id      = var.route_table_id
-  destination_cidr_block = var.destination_cidr_block
-  transit_gateway_id  = aws_ec2_transit_gateway.transit_gateway.id
+resource "aws_route" "this" {
+  provider               = aws.target
+  for_each               = toset(var.route_table_ids)
+  route_table_id         = each.value
+  destination_cidr_block = var.source_cidr_block
+  transit_gateway_id     = aws_ec2_transit_gateway.transit_gateway.id
 
-  depends_on = [aws_ec2_transit_gateway_vpc_attachment.example]
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.this]
 }

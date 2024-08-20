beb0f8dcc1bd01584b6e6ec70a6a24a3cf2ef7ca
Manikanta Sadurla <95267560+manikanta-sadurla@users.noreply.github.com>
2024-08-20 18:36:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ terraform {
     aws = {
       version = "~> 4.0"
       source  = "hashicorp/aws"
+      configuration_aliases = [aws.target]
     }
 
     awsutils = {
@@ -14,26 +15,26 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
-provider "awsutils" {
-  region = var.region
-}
+# provider "awsutils" {
+#   region = var.region
+# }
 
-provider "aws" {
-  alias  = "target"
-  region = var.region
-  assume_role {
-    role_arn = var.assume_role_arn
-  }
-}
+# provider "aws" {
+#   alias  = "target"
+#   region = var.region
+#   assume_role {
+#     role_arn = var.assume_role_arn
+#   }
+# }
 
-##################### Source Account #################
-# Existing Transit Gateway in the source account
+
+# Conditional creation of Transit Gateway
 resource "aws_ec2_transit_gateway" "transit_gateway" {
-  provider                        = aws
+  count                           = var.create_transit_gateway ? 1 : 0
   amazon_side_asn                 = var.transit_gateway_asn
   auto_accept_shared_attachments  = "enable"
   default_route_table_association = "enable"
@@ -46,7 +47,7 @@ resource "aws_ec2_transit_gateway" "transit_gateway" {
   }
 }
 
-# Sharing the Transit Gateway via RAM
+# Share the Transit Gateway via RAM
 resource "aws_ram_resource_share" "transit_gateway_share" {
   name                      = "transit-gateway-share"
   allow_external_principals = true
@@ -57,45 +58,52 @@ resource "aws_ram_resource_share" "transit_gateway_share" {
 }
 
 resource "aws_ram_principal_association" "target_account" {
+  for_each               = toset(var.target_account_id)
   resource_share_arn = aws_ram_resource_share.transit_gateway_share.arn
-  principal          = var.target_account_id
+  principal          = each.value
 }
 
 resource "aws_ram_resource_association" "transit_gateway" {
   resource_share_arn = aws_ram_resource_share.transit_gateway_share.arn
-  resource_arn       = aws_ec2_transit_gateway.transit_gateway.arn
+  resource_arn       = var.create_transit_gateway ? aws_ec2_transit_gateway.transit_gateway[0].arn : var.existing_transit_gateway_arn
 }
 
-
 resource "aws_ec2_transit_gateway_vpc_attachment" "source" {
-
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
+  transit_gateway_id = var.create_transit_gateway ? aws_ec2_transit_gateway.transit_gateway[0].id : var.existing_transit_gateway_id
   vpc_id             = var.source_vpc_id
-
-  subnet_ids = var.source_subnet_ids
-
-  dns_support  = "enable"
-  ipv6_support = "disable"
+  subnet_ids         = var.source_subnet_ids
+  dns_support        = "enable"
+  ipv6_support       = "disable"
 
   tags = {
     Name = "TransitGateway-VPC-Attachment"
   }
-  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+
+  depends_on = [
+    aws_ec2_transit_gateway.transit_gateway,
+    aws_ram_principal_association.target_account,
+    aws_ram_resource_association.transit_gateway
+  ]
+}
+
+
+resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "source" {
+  count                         = var.create_transit_gateway ? 0 : 1
+#   provider                      = aws
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.this.id
 }
 
 resource "aws_route" "source" {
   for_each               = toset(var.source_route_table_ids)
   route_table_id         = each.value
   destination_cidr_block = var.destination_cidr_block
-  transit_gateway_id     = aws_ec2_transit_gateway.transit_gateway.id
+  transit_gateway_id     = var.create_transit_gateway ? aws_ec2_transit_gateway.transit_gateway[0].id : var.existing_transit_gateway_id
 
   depends_on = [aws_ec2_transit_gateway_vpc_attachment.source]
 }
 
-
-################### Target Acconunt #####################################
-# Accept the share in the target account
-#########################################################################
+####################################################################
+# Target Account resources
 resource "aws_ram_resource_share_accepter" "transit_gateway" {
   provider  = aws.target
   share_arn = aws_ram_resource_share.transit_gateway_share.arn
@@ -105,26 +113,30 @@ resource "aws_ram_resource_share_accepter" "transit_gateway" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   provider           = aws.target
-  transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
+  transit_gateway_id = var.create_transit_gateway ? aws_ec2_transit_gateway.transit_gateway[0].id : var.existing_transit_gateway_id
   vpc_id             = var.vpc_id
-
-  subnet_ids = var.subnet_ids
-
-  dns_support  = "enable"
-  ipv6_support = "disable"
+  subnet_ids         = var.subnet_ids
+  dns_support        = "enable"
+  ipv6_support       = "disable"
 
   tags = {
     Name = "TransitGateway-VPC-Attachment"
   }
-  depends_on = [aws_ec2_transit_gateway.transit_gateway]
+
+  depends_on = [
+    aws_ec2_transit_gateway.transit_gateway,
+    aws_ram_principal_association.target_account,
+    aws_ram_resource_association.transit_gateway
+  ]
 }
+
 
 resource "aws_route" "this" {
   provider               = aws.target
   for_each               = toset(var.route_table_ids)
   route_table_id         = each.value
   destination_cidr_block = var.source_cidr_block
-  transit_gateway_id     = aws_ec2_transit_gateway.transit_gateway.id
+  transit_gateway_id     = var.create_transit_gateway ? aws_ec2_transit_gateway.transit_gateway[0].id : var.existing_transit_gateway_id
 
   depends_on = [aws_ec2_transit_gateway_vpc_attachment.this]
 }

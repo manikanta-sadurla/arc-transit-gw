a24f95f3907c694d01bfb0e395b26475c2cfe2c0
Manikanta Sadurla <95267560+manikanta-sadurla@users.noreply.github.com>
2024-08-23 18:05:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,37 +1,17 @@
-# ################################################################
-# ## defaults
-# ################################################################
-# terraform {
-#   required_version = "~> 1.3"
+################################################################################
+## defaults
+################################################################################
+terraform {
+  required_version = "~> 1.3, < 2.0.0"
 
-#   required_providers {
-#     aws = {
-#       version = "~> 4.0"
-#       source  = "hashicorp/aws"
-#     }
-
-#     awsutils = {
-#       source  = "cloudposse/awsutils"
-#       version = "~> 0.15"
-#     }
-#   }
-
-# }
-
-# provider "aws" {
-#   region = "us-east-1"
-# #   assume_role {
-# #     role_arn = local.environment_role[var.environment]
-# #   }
-# #   default_tags {
-# #     tags = module.tags.tags
-# #   }
-# }
-
-# provider "awsutils" {
-#   region = var.region
-# #   assume_role {
-# #     role_arn = local.environment_role[var.environment]
-# #   }
-# }
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
